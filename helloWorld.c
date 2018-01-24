af06afed75c7bb736d3f96721d9464b0dc4daef1
WonChoi <acetopwon@gmail.com>
2018-01-23 21:20:55 -0600

    my name prints
    I wrote  a program that will spit out my name. Nick add your name on to this thank you.

---
+++ helloWorld.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Won Choi, Nick Nyholm
+ * Date: January 20, 2018
+ *
+ * This program is just like hello world except it will print out my name and my partner's name instead.
+ */
+ #include<stdio.h>
+ #include<stdlib.h>
+ 
+ int main (int argc, char **argv) {
+	 
+	 printf("WonHyeok Choi, \n") ;
+	 
+	 return 0 ;
+	 
+ }
